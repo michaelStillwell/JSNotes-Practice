34f32800853a5624c222909402923345e42f329d
Michael <michael.stillwell@hotmail.com>
2018-01-24 08:53:01 -0600

    Test commit

---
+++ test.js
@@ -0,0 +1,42 @@
+// var materials = [
+//     'Hydrogen',
+//     'Helium',
+//     'Lithium',
+//     'Beryllium'
+//   ];
+    
+//   materials.map(function(material) { 
+//     return material.length; 
+//   }); // [8, 6, 7, 9]
+  
+//   materials.map((material) => {
+//     return material.length;
+//   }); // [8, 6, 7, 9]
+  
+//   materials.map(material => material.length); // [8, 6, 7, 9]
+
+
+//   var myNumbers = [ 2, 5, 6 ];
+//   var doubled   = myNumbers.forEach( function (num, i, arr) {
+//     return arr[i] *= 2;
+//   })
+
+//   console.log( doubled );
+
+//   var colorResponse = "red";
+  
+//   var responseCreator = function(param) {
+//     // CODE HERE
+//     if ( param = "green" ) {
+//       return "Green is okay.";
+//     } else if ( param = "red" ) {
+//       return "I heart red.";
+//     } else { 
+//       return "What is your favorite color?";
+//     }
+//     return param;
+//   }
+
+
+
+//   console.log( responseCreator(colorResponse) );
